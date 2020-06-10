f6af400db134f35765a69a5dfa7ab5753eea9d4b
Danny Wood <danny@inspiredagency.co.uk>
2020-06-10 17:47:04 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Unslugify
+
+Unslugify your text
+
+# Installation
+
+`npm i unslugify`
+
+Then...
+
+```
+import { unslugify } from 'unslugify';
+
+const result = unslugify('my-example-slug');
+```
+
+## Options
+
+TODO
---
+++ index.js
@@ -0,0 +1,8 @@
+function unslugify(slug) {
+  const result = slug.replace(/\-/g, " ");
+  return result.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
+module.exports.unslugify = unslugify;
